4ef6e1eab166c45dca0c496cca6a0e6508908b0a
Cliff Brake <cbrake@bec-systems.com>
2015-06-19 23:02:56 -0400

    enumerate wemo devices and send status to server

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 #!/usr/bin/env node
 
-var mqtt    = require('mqtt');
-var client  = mqtt.connect('mqtt://test.mosquitto.org');
+var config = require('./config');
+var mqtt = require('mqtt');
+var wemo = require('wemo');
+
+var mqttClient  = mqtt.connect(config.mqttServer);
  
-client.on('connect', function () {
-  client.subscribe('presence');
-  //client.publish('presence', 'Hello mqtt');
+mqttClient.on('connect', function () {
+  mqttClient.subscribe('presence');
+  mqttClient.publish('presence', 'Hello mqtt');
 });
  
-client.on('message', function (topic, message) {
+mqttClient.on('message', function (topic, message) {
   // message is Buffer 
   console.log(message.toString());
-  client.end();
 });
+
+wemoClient = wemo.Search();
+
+var wemoDevices = {};
+
+wemoClient.on('found', function(device) {
+  console.log(device);
+  wemoDevices[device.friendlyName] = device;
+});
+
+setInterval(function() {
+  for (var name in wemoDevices) {
+    var d = wemoDevices[name];
+    var wemoSwitch = new wemo(d.ip, d.port);
+    wemoSwitch.getBinaryState(function(err, result) {
+      if (err) {
+        console.log('Error reading ' + name + err);
+      } else {
+        mqttClient.publish('node/wemo/' + name, result);
+      }
+    });
+
+  }
+}, 3000);
+
 
 
